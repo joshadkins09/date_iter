5ef3c7aaa048b6702d1deab37de5bf52a55a8685
jadkins <joshadkins09@gmail.com>
2020-12-08 21:12:10 -0800

    interface updates

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,12 @@ extern crate chrono;
 use chrono::Duration;
 
 pub type Date = chrono::naive::NaiveDate;
-pub type DatePair = (Date, Date);
 
 /// Iterator for dates
 #[derive(Debug)]
 pub struct DateIter {
     cur: Date,
     stop: Date,
-}
-
-impl DateIter {
-    pub fn new(start: Date, stop: Date) -> Self {
-        Self {
-            cur: start,
-            stop: stop,
-        }
-    }
 }
 
 impl Iterator for DateIter {
@@ -38,6 +28,13 @@ impl Iterator for DateIter {
     }
 }
 
+pub fn date_iter(start: Date, stop: Date) -> DateIter {
+    DateIter {
+        cur: start,
+        stop: stop,
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #[cfg(test)]
@@ -46,21 +43,15 @@ mod tests {
 
     #[test]
     fn basic() {
-        let di = DateIter::new(
-            Date::from_ymd(2020, 12, 4),
-            Date::from_ymd(2020, 12, 8),
-        );
+        let di = date_iter(Date::from_ymd(2020, 12, 4), Date::from_ymd(2020, 12, 8));
         let actual: Vec<Date> = di.collect();
         let expected: Vec<Date> = (4..9).map(|d| Date::from_ymd(2020, 12, d)).collect();
         assert_eq!(actual, expected);
     }
 
     #[test]
-    fn interval() {
-        let di = DateIter::new(
-            Date::from_ymd(2020, 12, 1),
-            Date::from_ymd(2020, 12, 8),
-        );
+    fn step() {
+        let di = date_iter(Date::from_ymd(2020, 12, 1), Date::from_ymd(2020, 12, 8));
         let actual: Vec<Date> = di.step_by(2).collect();
         let expected: Vec<Date> = (1..8)
             .step_by(2)
